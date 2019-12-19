b36759d64aecad695be9a12f5935605ccc4af4d0
Sheng Kai Teo <skaiteo98@gmail.com>
2019-12-20 00:20:53 +0800

    First commit

---
+++ android/app/src/main/kotlin/com/example/red_tailed_hawk/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.red_tailed_hawk
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
