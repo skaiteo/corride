40892deacc03d60aafbc49395bf429c65f2f15ed
Timothy Low <tlow100@gmail.com>
2019-12-23 16:36:35 +0800

    Added Interface for Login Page

--- lib/main.dart
+++ lib/main.dart
@@ -11,103 +11,211 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.red,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  void _clearCounter(){
-    setState(() {
-      _counter = 0;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
+      body: SingleChildScrollView(
+        child: Stack(
           children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+            Positioned(
+              child: Image(
+                image: AssetImage("assets/images/login-background-image.png"),
+                fit: BoxFit.cover,
+                height: 800,
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
+            Positioned(
+              top: 0,
+              bottom: 0,
+              left: 0,
+              right: 0,
+              child: Container(
+                color: Color.fromRGBO(255, 160, 0, 0.7),
+              ),
             ),
-            RaisedButton(
-              child: Text("Clear", style: TextStyle(color: Colors.white),),
-              color: Colors.red,
-              onPressed: _clearCounter,
+            Positioned.fill(
+                top: 150,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Expanded(
+                      flex: 1,
+                      child: Align(
+                        alignment: Alignment.topCenter,
+                        child: Text(
+                          "CORRIDE",
+                          style: TextStyle(
+                              fontSize: 60,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white,
+                              shadows: [
+                                Shadow(
+                                    blurRadius: 10.0,
+                                    color: Color(0xFFC67100),
+                                    offset: Offset(5.0, 5.0))
+                              ]),
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                      child: loginCard(),
+                      flex: 6,
+                    )
+                  ],
+                )),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget loginCard() {
+    return Align(
+      child: Container(
+        height: 400,
+        width: 350,
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(15),
+          boxShadow: [
+            BoxShadow(
+              color: Color.fromRGBO(0, 0, 0, 0.5),
+              offset: Offset(0, 3),
+              blurRadius: 6,
+            )
+          ],
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            _commonButton(true),
+            Text("Or"),
+            _textInput(Icons.mail, "Email address", true),
+            TextField(),
+            _commonButton(false)
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+Widget _commonButton(bool hasIcon) {
+  if (hasIcon) {
+    return InkWell(
+      onTap: () {
+        print("Sign In Pressed");
+      },
+      child: Container(
+        height: 50,
+        width: 268,
+        decoration: BoxDecoration(
+          color: Color(0xFFFFA000),
+          borderRadius: BorderRadius.circular(15),
+        ),
+        child: Stack(
+          children: <Widget>[
+            Positioned(
+              child: Container(
+                  height: 50,
+                  width: 50,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(15),
+                    color: Color(0xFFC67100),
+                  ),
+                  child: Icon(
+                    Icons.person_outline,
+                    color: Colors.white,
+                  )),
+            ),
+            Positioned.fill(
+              left: 40,
+              child: Align(
+                alignment: Alignment.center,
+                child: Text(
+                  "Sign Up for New User",
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold),
+                ),
+              ),
             )
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  } else {
+    return InkWell(
+      onTap: () {
+        print("Login pressed");
+      },
+      child: Container(
+        height: 50,
+        width: 268,
+        decoration: BoxDecoration(
+          color: Color(0xFFC67100),
+          borderRadius: BorderRadius.circular(15),
+        ),
+        child: Align(
+          alignment: Alignment.center,
+          child: Text(
+            "Login",
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 16,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+      ),
     );
   }
+}
+
+Widget _textInput(IconData icon, String placeholder, bool isPassword) {
+  return Container(
+    height: 50.00,
+    width: 268.00,
+    decoration: BoxDecoration(
+      color: Color(0xffeeeeee),
+      borderRadius: BorderRadius.circular(15.00),
+    ),
+    child: Row(
+      children: <Widget>[
+        Container(
+          height: 50,
+          width: 50,
+          decoration: BoxDecoration(
+            color: Color(0xffD4D4D4),
+            borderRadius: BorderRadius.circular(15.00),
+          ),
+          child: Icon(
+            icon,
+            color: Color(0xff9E9E9E),
+          ),
+        ),
+        TextField(
+          decoration: InputDecoration(
+            labelText: placeholder,
+            labelStyle: TextStyle(
+                color: Color(0xff9E9E9E),
+                fontSize: 16,
+                fontWeight: FontWeight.bold),
+          ),
+        ),
+      ],
+    ),
+  );
 }
